6fd08151d546fc9dbea59737165a213d46eed68d
JulianKer <jschmuker742@alumno.unlam.edu.ar>
2024-02-14 23:34:49 -0300

    se agregan las task correctamente con la eliminacion del warning

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ let task = `<section class="task">
                 </div>
             </section>`;
 
-let warning =  `<section class="warning-o-tasks">
+let mensajeWarning =  `<section class="warning-no-tasks">
                     <img src="icon/fingerUp.svg" alt="finger-up" class="img-finger">
                     <h3 class="title-warning">¡No hay tareas pendientes!</h3>
                 </section>`
@@ -33,11 +33,86 @@ let selectFecha = document.getElementById("selector-fecha");
 
 // all task --------------------------
 let contenedorAllTask = document.getElementById("all-task");
+let arrayDeTasks = [];
 //------------------------------------
 
-console.log(inputDescripcion);
-console.log(selectEstadoNuevaTask);
-console.log(boton);
-console.log(selectFiltroPorEstados);
-console.log(selectFecha);
-console.log(contenedorAllTask);
+boton.addEventListener("click", ()=>{
+    eliminarWarning();
+
+    if (inputDescripcion.value != "") {
+        let descripcion = inputDescripcion.value;
+        let estado = selectEstadoNuevaTask.value;
+
+        switch (selectEstadoNuevaTask.value) {
+            case "status-por-empezar":
+                contenedorAllTask.innerHTML += `
+                <section class="task">
+                            <div class="first-row">
+                                <h4>Hoy</h4>
+                                <img src="icon/cerrar.png" alt="Eliminar" title="Eliminar" class="img-cruz">
+                            </div>
+                            <div class="second-row">
+                                <p class="texto-descriptivo">`+ descripcion + `</p>
+                                <div class="selector-estado-de-tarea">
+                                    <label for="estado-de-tarea">Estado:</label>
+                                    <select name="estado-de-tarea" id="estado-de-tarea">
+                                        <option value="estado-por-empezar" selected >Por empezar</option>
+                                        <option value="estado-en-proceso">En proceso</option>
+                                        <option value="estado-terminada">Terminada</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </section>`
+                break;
+            case "status-en-proceso":
+                contenedorAllTask.innerHTML += `
+                <section class="task">
+                            <div class="first-row">
+                                <h4>Hoy</h4>
+                                <img src="icon/cerrar.png" alt="Eliminar" title="Eliminar" class="img-cruz">
+                            </div>
+                            <div class="second-row">
+                                <p class="texto-descriptivo">`+ descripcion + `</p>
+                                <div class="selector-estado-de-tarea">
+                                    <label for="estado-de-tarea">Estado:</label>
+                                    <select name="estado-de-tarea" id="estado-de-tarea">
+                                        <option value="estado-por-empezar">Por empezar</option>
+                                        <option value="estado-en-proceso" selected>En proceso</option>
+                                        <option value="estado-terminada">Terminada</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </section>`
+                        
+                break;
+            case "status-terminada":
+                contenedorAllTask.innerHTML += `
+                <section class="task">
+                            <div class="first-row">
+                                <h4>Hoy</h4>
+                                <img src="icon/cerrar.png" alt="Eliminar" title="Eliminar" class="img-cruz">
+                            </div>
+                            <div class="second-row">
+                                <p class="texto-descriptivo">`+ descripcion + `</p>
+                                <div class="selector-estado-de-tarea">
+                                    <label for="estado-de-tarea">Estado:</label>
+                                    <select name="estado-de-tarea" id="estado-de-tarea">
+                                        <option value="estado-por-empezar">Por empezar</option>
+                                        <option value="estado-en-proceso">En proceso</option>
+                                        <option value="estado-terminada" selected>Terminada</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </section>`
+                break;
+        }
+        inputDescripcion.value = "";
+    }
+})
+
+function eliminarWarning(){
+    let sectionWarning = document.getElementById("warning-no-tasks");
+    if (sectionWarning) {
+        contenedorAllTask.removeChild(sectionWarning);
+    }
+}
