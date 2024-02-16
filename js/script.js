1609f5f7111ed46ec00cd544be1e00dfa2888f8e
JulianKer <jschmuker742@alumno.unlam.edu.ar>
2024-02-15 21:22:22 -0300

    refactorización a una función mostrarArray

--- js/script.js
+++ js/script.js
@@ -26,29 +26,44 @@ boton.addEventListener("click", ()=>{
     if (inputDescripcion.value === "" || inputDescripcion.value === " ") {
         inputDescripcion.focus();
     }else{
-        eliminarWarning();
+        eliminarWarning(); // elimino el cartel de que no hay tareas pendientes
 
-        let descripcion = inputDescripcion.value;
-        let estado = selectEstadoNuevaTask.value;
-        let nuevaTarea;
+        let descripcion = inputDescripcion.value; // agarro la descripcion
+        let estado = selectEstadoNuevaTask.value;// agarro el estado
 
-        switch (selectEstadoNuevaTask.value) {
+        let nuevaTarea = {   // creo la task
+            id: "tarea-" + obtenerSiguienteId(), // obtengo el siguiente id
+            descripcion: descripcion,
+            estado: estado
+        };
+
+        arrayDeTasks.push(nuevaTarea); // meto la task al array
+        mostrarArray(arrayDeTasks); // muestro el array (en el html)
+
+        inputDescripcion.value = ""; //vacío el input
+        inputDescripcion.focus();  // le sigo haciendo focus
+    }
+})
+
+
+
+
+// ---------------------- FUNCIONES ------------------------------
+function mostrarArray(arrayRecibido) { 
+
+    contenedorAllTask.innerHTML = ""; // vacio el contenedor de las tareas asi NO se duplican
+
+    arrayRecibido.forEach(elementoDelArray =>{ // por cada task,
+        switch (elementoDelArray.estado) {   // veo segun su estado para poner el "selected"
             case "status-por-empezar":
-                nuevaTarea = {
-                    id: "tarea-" + obtenerSiguienteId(),
-                    descripcion: descripcion,
-                    estado: estado
-                };
-                arrayDeTasks.push(nuevaTarea);
-
                 contenedorAllTask.innerHTML += `
-                                    <section class="task" id="` + nuevaTarea.id + `">
+                                    <section class="task" id="` + elementoDelArray.id + `">
                                         <div class="first-row">
                                             <h4>Hoy</h4>
-                                            <img src="icon/cerrar.png" alt="Eliminar" title="Eliminar" class="img-cruz" id="`+ nuevaTarea.id +`">
+                                            <img src="icon/cerrar.png" alt="Eliminar" title="Eliminar" class="img-cruz" id="`+ elementoDelArray.id +`">
                                         </div>
                                         <div class="second-row">
-                                            <p class="texto-descriptivo">`+ nuevaTarea.descripcion + `</p>
+                                            <p class="texto-descriptivo">`+ elementoDelArray.descripcion + `</p>
                                             <div class="selector-estado-de-tarea">
                                                 <label for="estado-de-tarea">Estado:</label>
                                                 <select name="estado-de-tarea" id="estado-de-tarea">
@@ -61,21 +76,14 @@ boton.addEventListener("click", ()=>{
                                     </section>`
                     break;
                 case "status-en-proceso":
-                    nuevaTarea = {
-                        id: "tarea-" + (arrayDeTasks.length+1),
-                        descripcion: descripcion,
-                        estado: estado
-                    };
-                    arrayDeTasks.push(nuevaTarea);
-    
                     contenedorAllTask.innerHTML += `
-                                        <section class="task" id="` + nuevaTarea.id + `">
+                                        <section class="task" id="` + elementoDelArray.id + `">
                                             <div class="first-row">
                                                 <h4>Hoy</h4>
-                                                <img src="icon/cerrar.png" alt="Eliminar" title="Eliminar" class="img-cruz" id="`+ nuevaTarea.id +`">
+                                                <img src="icon/cerrar.png" alt="Eliminar" title="Eliminar" class="img-cruz" id="`+ elementoDelArray.id +`">
                                             </div>
                                             <div class="second-row">
-                                                <p class="texto-descriptivo">`+ nuevaTarea.descripcion + `</p>
+                                                <p class="texto-descriptivo">`+ elementoDelArray.descripcion + `</p>
                                                 <div class="selector-estado-de-tarea">
                                                     <label for="estado-de-tarea">Estado:</label>
                                                     <select name="estado-de-tarea" id="estado-de-tarea">
@@ -88,21 +96,14 @@ boton.addEventListener("click", ()=>{
                                         </section>`
                         break;
                 case "status-terminada":
-                    nuevaTarea = {
-                        id: "tarea-" + (arrayDeTasks.length+1),
-                        descripcion: descripcion,
-                        estado: estado
-                    };
-                    arrayDeTasks.push(nuevaTarea);
-    
                     contenedorAllTask.innerHTML += `
-                                        <section class="task" id="` + nuevaTarea.id + `">
+                                        <section class="task" id="` + elementoDelArray.id + `">
                                             <div class="first-row">
                                                 <h4>Hoy</h4>
-                                                <img src="icon/cerrar.png" alt="Eliminar" title="Eliminar" class="img-cruz" id="`+ nuevaTarea.id +`">
+                                                <img src="icon/cerrar.png" alt="Eliminar" title="Eliminar" class="img-cruz" id="`+ elementoDelArray.id +`">
                                             </div>
                                             <div class="second-row">
-                                                <p class="texto-descriptivo">`+ nuevaTarea.descripcion + `</p>
+                                                <p class="texto-descriptivo">`+ elementoDelArray.descripcion + `</p>
                                                 <div class="selector-estado-de-tarea">
                                                     <label for="estado-de-tarea">Estado:</label>
                                                     <select name="estado-de-tarea" id="estado-de-tarea">
@@ -115,86 +116,39 @@ boton.addEventListener("click", ()=>{
                                         </section>`
                         break;
                 }
-        inputDescripcion.value = "";
-        inputDescripcion.focus()
+            })
 
-        arrayDeTasks.forEach(element => {
-            console.log(element.id)
-        });
-        console.log("......")
+    botonesEliminarTask = document.querySelectorAll(".img-cruz")
 
-        botonesEliminarTask = document.querySelectorAll(".img-cruz")
+    botonesEliminarTask.forEach(element => {
+        element.addEventListener("click", ()=>{
 
-        botonesEliminarTask.forEach(element => {
+            let idDelElemento = element.id;
 
-            element.addEventListener("click", ()=>{
+            arrayRecibido.forEach(task=>{
 
-                let idDelElemento = element.id;
+                if (task.id === idDelElemento) {
+                    let taskAEliminar = document.getElementById(idDelElemento);
 
-                arrayDeTasks.forEach(task=>{
+                    if (taskAEliminar != null) {
+                        contenedorAllTask.removeChild(taskAEliminar); // lo remuevo del html
+                        let indexAEliminar = arrayRecibido.findIndex(element => element.id === idDelElemento) // busco el index del elemento q quiero eliminar
+                        arrayRecibido.splice(indexAEliminar,1) // lo elimino del array
 
-                    if (task.id === idDelElemento) {
-                        let taskAEliminar = document.getElementById(idDelElemento);
-
-                        if (taskAEliminar != null) {
-                            contenedorAllTask.removeChild(taskAEliminar); // lo remuevo del html
-                            let indexAEliminar = arrayDeTasks.findIndex(element => element.id === idDelElemento) // busco el index del elemento q quiero eliminar
-                            arrayDeTasks.splice(indexAEliminar,1) // lo elimino del array
-
-                            console.log("tamanio del array: " + arrayDeTasks.length)
-                            if (arrayDeTasks.length === 0) {
-                                mostrarWarning();
-                            }
-
-                            // las task se eliminan correctamente pero ver mejor cuando se le aplica el id, que SIEMPRE sea NO del lenght sino del 
-                            // ultimo elemento mas 1 pq asi no se repiten nunca, osea tratar de que si tengo 10 elemntos, no hacer el
-                            // lenght mas 1 sino buscar cual es el ultimo y ai sumar uno pq capa el ultimo es de id 5 pero esta solo ese elemento
-                            // entonces desp se va a repetir LO HICE EN UN METODO MAS ABAJO, MIRAR
-
-                            // SINO LO PODRIA DEJAR ASI QUE YA SE MUESTRA PERO BUENO CAPAZ DESPUES SE ME COMPLICA A LA HORA DE ORDENARLOS Y ESO
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                        console.log("tamanio del array: " + arrayRecibido.length)
+                        if (arrayRecibido.length === 0) {
+                            mostrarWarning();
                         }
                     }
-                })
+                }
             })
-        });
-    }
-})
+        })
+    });
+}
 
 function obtenerSiguienteId() {
     return (++id);
 }
-
 
 function eliminarWarning(){
     let sectionWarning = document.getElementById("warning-no-tasks");
